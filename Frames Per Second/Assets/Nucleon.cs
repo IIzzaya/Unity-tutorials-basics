640f286b19cdce54a303ca352480dc45ff0acbe4
IIzzaya <linxuan_0709@icloud.com>
2018-05-18 15:57:41 +0800

    Update Chapter "Frames Per Second"

---
+++ Frames Per Second/Assets/Nucleon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+
+	public float attractionForce;
+	Rigidbody body;
+
+	private void Awake() {
+		body = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		body.AddForce(transform.localPosition * -attractionForce);
+	}
+}
---
+++ Frames Per Second/Assets/NucleonSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+	public float timeGap;
+	public float spawnDistance;
+
+	public Nucleon[] nucleonPrefabs;
+
+	void SpawnNucleon(int index)
+	{
+		Nucleon spawn = Instantiate(nucleonPrefabs[index]);
+		spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+	}
+	float timeSinceLastSpawn;
+	void FixedUpdate()
+	{
+		timeSinceLastSpawn += Time.deltaTime;
+		if (timeSinceLastSpawn >= timeGap)
+		{
+			timeSinceLastSpawn -= timeGap;
+			SpawnNucleon(Random.Range(0, nucleonPrefabs.Length));
+		}
+
+	}
+}
